b82c1dfbc8fd81692be31641e8b89dd8c1b21e39
Marcus Pousette <marcus.pousette@quantleaf.com>
2022-12-08 18:52:30 +0100

    Updates

---
+++ static/js/636.5df45d0a.chunk.js
@@ -0,0 +1,11 @@
+(self["webpackChunkfrontend"] = self["webpackChunkfrontend"] || []).push([[636],{
+
+/***/ 42636:
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
+//# sourceMappingURL=636.5df45d0a.chunk.js.map
