e7fd2e91e0890aec2437ddf962eb5b5d94b2fcaf
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-02-20 19:51:11 -0800

    build demo

--- packages/live-streaming/frontend/src/index.tsx
+++ packages/live-streaming/frontend/src/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
-
+import * as s from "./iframeResizer.contentWindow.min.js";
+!!s;
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
