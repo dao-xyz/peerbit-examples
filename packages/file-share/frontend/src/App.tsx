764fb533b2510b5dcae6b011a60b7dbceb5203d1
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-09-25 11:24:44 +0200

    update bootstrap

--- packages/file-share/frontend/src/App.tsx
+++ packages/file-share/frontend/src/App.tsx
@@ -14,9 +14,6 @@ export const App = () => {
                     import.meta.env.MODE === "development" ? "local" : "remote",
                 host: true,
                 waitForConnnected: true,
-                bootstrap: [
-                    "/dns4/8f7ec389599f3350e466dbcbd11acea60c3e3376.peerchecker.com/tcp/4003/wss/p2p/12D3KooWGERhPmCQ4GpxL1rX5y2uEJENcaShygapvUmnpfoddypy",
-                ],
             }}
         >
             <div className="h-screen">
