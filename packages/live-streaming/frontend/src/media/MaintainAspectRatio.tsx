03607a8439965912e898c66872e493f4bf5f7971
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-02-08 00:04:14 +0100

    fix: maintain aspect ratio

---
+++ packages/live-streaming/frontend/src/media/MaintainAspectRatio.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+export const getKeepAspectRatioBoundedSize = ({
+    width,
+    height,
+}: {
+    width: number;
+    height: number;
+}) => {
+    const [styleHeight, setStyleHeight] = useState<"100dvh" | "fit-content">(
+        "fit-content"
+    );
+    const [styleWidth, setStyleWidth] = useState<"100dvw" | "fit-content">(
+        "100dvw"
+    );
+
+    useEffect(() => {
+        const listener = () => {
+            const containerSize: { width: number; height: number } = {
+                width: window.innerWidth,
+                height: window.innerHeight,
+            };
+            //   console.log(width / containerSize.width, height / containerSize.height)
+            const isLimitedByHeight =
+                width / containerSize.width < height / containerSize.height;
+            console.log("??", isLimitedByHeight, width);
+
+            if (isLimitedByHeight) {
+                setStyleHeight("100dvh");
+                setStyleWidth("fit-content");
+            } else {
+                setStyleHeight("fit-content");
+                setStyleWidth("100dvw");
+            }
+        };
+        window.addEventListener("resize", listener);
+        return () => window.removeEventListener("resize", listener);
+    });
+    return { width: styleWidth, height: styleHeight };
+};
