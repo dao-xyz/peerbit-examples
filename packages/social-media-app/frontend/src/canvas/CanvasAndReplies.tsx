74f9566212a23375d2d14eb2365e5bea507f8c4e
Benjamin Preiss <benjaminpreiss@protonmail.com>
2025-03-16 13:37:33 +0100

    fix: remove console log

--- packages/social-media-app/frontend/src/canvas/CanvasAndReplies.tsx
+++ packages/social-media-app/frontend/src/canvas/CanvasAndReplies.tsx
@@ -60,7 +60,6 @@ export const CanvasAndReplies = () => {
                     scrollContainerRef.current &&
                     bottomScrollMarkerRef.current
                 ) {
-                    console.log("saving");
                     const scrollContainer = scrollContainerRef.current;
                     const bottomScrollMarkerContainer =
                         bottomScrollMarkerRef.current;
