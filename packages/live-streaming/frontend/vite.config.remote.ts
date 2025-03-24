5d0f4471c7070c5cfd648b408dc373b41c7caf2d
Marcus Pousette <marcus.pousette@quantleaf.com>
2025-03-24 19:35:11 +0100

    fix: rm unecessary config

--- packages/live-streaming/frontend/vite.config.remote.ts
+++ packages/live-streaming/frontend/vite.config.remote.ts
@@ -13,7 +13,6 @@ export default defineConfig({
         esbuildOptions: {
             target: "esnext",
         },
-        exclude: ["@peerbit/any-store"],
     },
     build: {
         target: "esnext",
