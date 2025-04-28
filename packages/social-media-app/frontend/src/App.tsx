3993851b587ee8deb6610b0e8cbf49e3a4afa042
Marcus Pousette <marcus.pousette@quantleaf.com>
2025-04-28 14:15:13 +0200

    fix: custom bootstrap

--- packages/social-media-app/frontend/src/App.tsx
+++ packages/social-media-app/frontend/src/App.tsx
@@ -98,6 +98,9 @@ const networkConfig: NetworkOption =
           }
         : {
               type: "remote",
+              bootstrap: [
+                  "/dns4/30c401611e3fe096fb0786dc1a0c9a3fd4057df4.peerchecker.com/tcp/4003/wss/p2p/12D3KooWCt4N9oVUmKVVA3Dq4goX1rRJJU8TtXGmCXXExrks87e9",
+              ],
           };
 
 export const App = () => {
