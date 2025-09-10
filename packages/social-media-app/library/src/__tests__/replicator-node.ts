3683482f301b21171f3c370cae58478020150471
Marcus Pousette <marcus.pousette@quantleaf.com>
2025-09-10 11:52:32 +0200

    fix: test replicator

--- packages/social-media-app/library/src/__tests__/replicator-node.ts
+++ packages/social-media-app/library/src/__tests__/replicator-node.ts
@@ -19,9 +19,24 @@ console.log(
     StaticPartialImage
 );
 const client = await Peerbit.create();
-console.log("Client created: ", client.identity.publicKey.hashcode());
-const localPeerId = await (await fetch("http://localhost:8082/peer/id")).text();
-await client.dial("/ip4/127.0.0.1/tcp/8002/ws/p2p/" + localPeerId);
+console.log("Client created");
+console.log("PeerId:", client.peerId.toString());
+console.log(
+    "Multiaddrs:",
+    client
+        .getMultiaddrs()
+        .map((a) => a.toString())
+        .join(", ")
+);
+// dial locally if env variable is set
+const dial = process.env.DIAL_LOCAL === "true" || false;
+if (dial) {
+    const localPeerId = await (
+        await fetch("http://localhost:8082/peer/id")
+    ).text();
+    await client.dial("/ip4/127.0.0.1/tcp/8002/ws/p2p/" + localPeerId);
+}
+
 const { scope: capsule, canvas: root } = await createRoot(client, {
     persisted: true,
 });
