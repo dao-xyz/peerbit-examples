566cd8a74e648a9197e4b1c6bae61d89b7972e79
Marcus Pousette <marcus.pousette@live.com>
2025-09-25 23:34:19 +0200

    fix: update bootstrap address

--- packages/social-media-app/frontend/src/bootstrap.ts
+++ packages/social-media-app/frontend/src/bootstrap.ts
@@ -1,4 +1,4 @@
 // keep the array in one place:
 export const BOOTSTRAP_ADDRS = [
-    "/dns4/c111240765c52526ebd787f2bd3901246fdc0d02.peerchecker.com/tcp/4003/wss/p2p/12D3KooWKXn15fY9aruL1CV48RUfaFsa4nYFQHAbMu6p2DhKdN4G",
+    "/dns4/c442cb473e627c06413b604bbd2a3a5af145c6a2.peerchecker.com/tcp/4003/wss/p2p/12D3KooWLZ5QJ27xaxBMaw76pZLYNB1LQ6jWbDpS8FXoS8EQk5aV",
 ];
